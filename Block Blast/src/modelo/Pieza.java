ed93fb0a44dbad0601f418100dd0f98f8623364a
HugoCanas <hugobikkia@gmail.com>
2026-04-16 13:42:15 +0200

    Cambios actualizados Block Blast

--- Block Blast/src/modelo/Pieza.java
+++ Block Blast/src/modelo/Pieza.java
@@ -12,32 +12,33 @@ public class Pieza {
 			Color.GREEN,
 			Color.MAGENTA
 	};
- 
+
 	private static final int[][][] FORMAS = {
-				    {{1,0,0},{0,0,0},{0,0,0}},
-				    {{1,1,0},{0,0,0},{0,0,0}},// línea horizontal de 2
-				    {{1,1,1},{0,0,0},{0,0,0}},// línea horizontal de 3
-				    {{1,0,0},{1,0,0},{0,0,0}},// línea vertical de 2
-				    {{1,0,0},{1,0,0},{1,0,0}},// línea vertical de 3
-				    {{1,1,0},{1,1,0},{0,0,0}},// cuadrado 2x2
-				    {{1,1,1},{1,1,1},{1,1,1}},// cuadrado 3x3
-				    {{1,0,0},{1,0,0},{1,1,0}},// L pequeña normal
-				    {{0,1,0},{0,1,0},{1,1,0}},// L pequeña invertida
-				    {{1,1,0},{1,0,0},{1,0,0}},// J pequeña normal
-				    {{1,1,0},{0,1,0},{0,1,0}},// J pequeña invertida
-				    {{1,1,1},{0,1,0},{0,0,0}},// forma t
-				    {{0,1,1},{1,1,0},{0,0,0}},// forma s
-				    {{1,1,0},{0,1,1},{0,0,0}},// forma z
-		};	
-	     
+			{{1,0,0},{0,0,0},{0,0,0}},
+			{{1,1,0},{0,0,0},{0,0,0}},// línea horizontal de 2
+			{{1,1,1},{0,0,0},{0,0,0}},// línea horizontal de 3
+			{{1,0,0},{1,0,0},{0,0,0}},// línea vertical de 2
+			{{1,0,0},{1,0,0},{1,0,0}},// línea vertical de 3
+			{{1,1,0},{1,1,0},{0,0,0}},// cuadrado 2x2
+			{{1,1,1},{1,1,1},{1,1,1}},// cuadrado 3x3
+			{{1,0,0},{1,0,0},{1,1,0}},// L pequeña normal
+			{{0,1,0},{0,1,0},{1,1,0}},// L pequeña invertida
+			{{1,1,0},{1,0,0},{1,0,0}},// J pequeña normal
+			{{1,1,0},{0,1,0},{0,1,0}},// J pequeña invertida
+			{{1,1,1},{0,1,0},{0,0,0}},// forma t
+			{{0,1,1},{1,1,0},{0,0,0}},// forma s
+			{{1,1,0},{0,1,1},{0,0,0}},// forma z
+	};	
+
 	private int [][] forma;
 	private Color color;
-	
+
 	//HACER AQUI TODO EL PROCESO DE LAS FORMAS DIFERENTES
 
-	public Pieza(int[][] forma) {
-		this.forma = forma;
-		this.color = COLORES[new Random().nextInt(COLORES.length)];
+	public Pieza() {
+		Random r = new Random();
+		this.forma = FORMAS[r.nextInt(FORMAS.length)];
+		this.color = COLORES[r.nextInt(COLORES.length)];
 	}
 
 	public int[][] getForma() {
